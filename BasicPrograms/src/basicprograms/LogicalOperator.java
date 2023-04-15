91251236f2f20d4bc33a362e66de349e7b1e3343
vedantBCodes <bichkundevedant97@gmail.com>
2023-04-14 20:26:08 -0700

    new java program added

---
+++ BasicPrograms/src/basicprograms/LogicalOperator.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basicprograms;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class LogicalOperator {
+     public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        System.out.println((a>b)&&(c>b));
+        System.out.println((a>b)||(c>b));
+         System.out.println(!(a>b));
+    }
+
+}
