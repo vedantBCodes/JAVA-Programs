dd6ca98a4295c4ce0426290f61eb6cbf8cc3880a
vedantBCodes <bichkundevedant97@gmail.com>
2023-04-15 20:29:48 -0700

    new programs added

---
+++ SpecialJavaPrograms/src/main/java/programs/specialjavaprograms/SpecialJavaPrograms.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package programs.specialjavaprograms;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class SpecialJavaPrograms {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
