3c0b0c5abe889d98b770e7a73527213627fd33a4
vedant Yevatikar <96629752+vedantBCodes@users.noreply.github.com>
2023-05-31 09:55:37 +0530

    program modified

--- StringPrograms/src/main/java/programs/stringprograms/StringCompair.java
+++ StringPrograms/src/main/java/programs/stringprograms/StringCompair.java
@@ -10,6 +10,8 @@ public class StringCompair {
         String str1=obj.nextLine();
         System.out.println("Enter another string:");
         String str2=obj.nextLine();
+        if(str1.length()==str2.length())
+        {
         for(int i=0;i<str2.length();i++)
         {
             if(str1.charAt(i)!=str2.charAt(i))
@@ -19,6 +21,11 @@ public class StringCompair {
             }
         }
         System.out.println("Both entered strings are same");
+        }
+        else
+        {
+            System.out.println("Strings are different");
+        }
     }
     
 }
